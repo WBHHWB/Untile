cfc6a2e90a407820357fe434eedb5e058f97a903
HLL <HLL@163.com>
2022-05-28 20:28:28 +0800

    第一次提交hot-fix分支

--- src/main/java/Package/sasd.java
+++ src/main/java/Package/sasd.java
@@ -8,5 +8,6 @@ public class sasd {
     public static void main(String[] args) {
         System.out.println("你好！");
         System.out.println("二次你好！");
+        System.out.println("你好hot-fix！");
     }
 }
