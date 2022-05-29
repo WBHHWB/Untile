d502ef86fa19316f3c2ca064514b2dd8955f4f53
HLL <HLL@163.com>
2022-05-29 19:49:54 +0800

    提交改了名字的主函数文件

--- src/main/java/Package/Claaaases.java
+++ src/main/java/Package/Claaaases.java
@@ -4,7 +4,7 @@ package Package;
  * @author 侯林林
  * 2022-05-28 19:38
  */
-public class Classes {
+public class Claaaases {
     public static void main(String[] args) {
         System.out.println("hello main！");
     }
